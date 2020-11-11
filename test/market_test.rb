9984721ce3b1daf3158097838d4e42ff48dcde82
Robert Heath <robert.b.heathii@gmail.com>
2020-11-11 11:15:15 -0600

    Test overstocked

--- test/market_test.rb
+++ test/market_test.rb
@@ -83,6 +83,8 @@ class MarketTest < Minitest::Test
     market.add_vendor(vendor2)
     market.add_vendor(vendor3)
 
+    assert_equal [item1, item2, item4, item3], market.item_keys
+
     expected = {
         item1 => {
           quantity: 100,
@@ -104,4 +106,27 @@ class MarketTest < Minitest::Test
     assert_equal expected, market.total_inventory
   end
 
+  def test_overstocked_items
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    vendor3.stock(item3, 10)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    assert_equal [item1], market.overstocked_items
+  end
 end
