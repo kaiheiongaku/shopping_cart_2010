e26cd54a0c33bd073dfa927ecbc7b2025b3b765d
Robert Heath <robert.b.heathii@gmail.com>
2020-11-11 11:13:40 -0600

    Code total_inventory

--- lib/market.rb
+++ lib/market.rb
@@ -23,4 +23,22 @@ class Market
       vendor.inventory.include?(item)
     end
   end
+
+  def item_keys
+    @vendors.flat_map do |vendor|
+      vendor.inventory.keys
+    end.uniq
+  end
+
+  def total_inventory
+    breakdown = Hash[self.item_keys.collect {|item| [item, {quantity: 0, vendors: []}]}]
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        breakdown[item][:quantity] += quantity
+        breakdown[item][:vendors] << vendor
+      end
+    end
+    breakdown
+  end
+
 end
