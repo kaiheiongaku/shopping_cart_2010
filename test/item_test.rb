f059704e6bbbba35275e7c6beaaab58072c9407c
Robert Heath <robert.b.heathii@gmail.com>
2020-11-11 10:06:26 -0600

    Initial test for item

---
+++ test/item_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal 'Peach', item1.name
+    assert_equal "$0.75", item1.price
+  end
+end
