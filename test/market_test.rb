e13f0abae21137629d552c2f489a9b14f7115699
Robert Heath <robert.b.heathii@gmail.com>
2020-11-11 11:41:17 -0600

    Test sorted item list

--- test/market_test.rb
+++ test/market_test.rb
@@ -107,6 +107,7 @@ class MarketTest < Minitest::Test
   end
 
   def test_overstocked_items
+    skip
     market = Market.new("South Pearl Street Farmers Market")
     vendor1 = Vendor.new("Rocky Mountain Fresh")
     vendor2 = Vendor.new("Ba-Nom-a-Nom")
@@ -128,5 +129,31 @@ class MarketTest < Minitest::Test
     market.add_vendor(vendor3)
 
     assert_equal [item1], market.overstocked_items
+  end
+
+  def test_sorted_items
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    vendor3.stock(item3, 10)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expected = ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
+
+    assert_equal expected, market.sorted_item_list
   end
 end
